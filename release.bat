0fad413b392c87df803f47bc49cecc0fe37ae365
AxyGitPoggers <supermarioglitchy95@gmail.com>
2021-09-03 13:22:13 +0100

    a

--- release.bat
+++ release.bat
@@ -1,1 +1,2 @@
-7z a -tzip -r -x!export -x!.git -x!.vscode -x!.gitignore -x!.travis.yml -x!release.bat flixel-demos.zip
+@echo off
+7z a -tzip -r -x!export -x!.git -x!.vscode -x!.gitignore -x!.travis.yml -x!release.bat flix.zip
